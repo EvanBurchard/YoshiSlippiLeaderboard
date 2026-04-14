e69a9307f942049ff02c91596f8ee4c8a26eab20
Evan Burchard <Evan@jades-mbp.mynetworksettings.com>
2026-04-14 16:31:00 -0400

    colors

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import { render } from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+const yoshiColors = ['#398123', '#b00033', '#1a2a5f', '#e1e54B', '#f08B8F', '#65d3dc'];
+const randomColor = yoshiColors[Math.floor(Math.random() * yoshiColors.length)];
+document.body.style.backgroundColor = randomColor;
+
 render(<App />, document.getElementById('app'));
